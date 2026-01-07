b8e8e311e8e3a19b6ac7a229558ff389d09721d4
ABDIEL <abdiel321atzin@gmail.com>
2026-01-06 20:47:03 -0600

    Se implementaron endpoints en Entidades para el CU gestionar entidades

--- src/routes/Entidades.Routes.js
+++ src/routes/Entidades.Routes.js
@@ -1,10 +1,15 @@
-import {Router} from 'express'
+import { Router } from 'express'
 import { EntidadesController } from '../controllers/Entidades.Controller.js'
+import { validarJWT } from '../middleware/ValidarJWT.js'
+import { validarAdmin } from '../middleware/ValidarAdmin.js'
 
 const router = Router()
 
-//Utilizar JWT y Middleware Admin
-router.post("/", EntidadesController.crearEntidad)    
+router.use(validarJWT, validarAdmin)
+
+router.post("/", EntidadesController.crearEntidad)
+router.get("/", EntidadesController.listarEntidades)
+router.get("/buscar", EntidadesController.buscarEntidades)
 router.get("/:id", EntidadesController.consultarEntidad)
 router.patch("/desactivar", EntidadesController.desactivarEntidad)
 router.patch("/reactivar", EntidadesController.reactivarEntidad)
